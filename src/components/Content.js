4c27d479b485e6bc8026563182dc9501678ee80b
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-11-12 11:42:06 +0600

    feat: lesson 17 done, contextType & useContext hook

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ThemeContext from '../contexts/themeContext';
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 
 export default function Content() {
+    const context = useContext(ThemeContext);
+    const { theme, switchTheme } = context;
     return (
         <div>
             <h1>This is a content</h1>
             <Counter>
                 {(count, incrementCount) => (
-                    <ThemeContext.Consumer>
-                        {({ theme }) => (
-                            <HoverCounter
-                                count={count}
-                                incrementCount={incrementCount}
-                                theme={theme}
-                            />
-                        )}
-                    </ThemeContext.Consumer>
+                    <HoverCounter
+                        count={count}
+                        incrementCount={incrementCount}
+                        theme={theme}
+                        switchTheme={switchTheme}
+                    />
                 )}
             </Counter>
         </div>
